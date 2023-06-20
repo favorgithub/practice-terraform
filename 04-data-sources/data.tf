f2145721b460c80ff5a59af98fdf9467b1c39468
favorgithub <gloify@yahoo.com>
2023-06-20 14:24:42 -0500

    Test sample yml

--- 04-data-sources/data.tf
+++ 04-data-sources/data.tf
@@ -1,3 +1,15 @@
+## Datasource is if you want to find out certain things that are already existing in your account in Amazon
+## The autentication to amazon also comes into picture because it has to autenticate to amaazon and get the things
+## Do a google search for the instance price terraform datasource to get the price of a particular instance
+## Any resource in terraform you will see the argument and Attribute
+## You have to look at the required argument and attribute
+## Argument means the things that you need to provide .e.g the instance type, avaialabily zone, filter
+## When the Argument is run, it expose some data and that data that it exposes is called attribute reference
+## search google for "aws security group data source terraform" we know the name of the security group but we want the id
+
+
+
+
 data "aws_ec2_spot_price" "example" {
   instance_type     = "t3.medium"
   availability_zone = "us-east-1a"
@@ -10,4 +22,12 @@ data "aws_ec2_spot_price" "example" {
 
 output "price" {
   value = data.aws_ec2_spot_price.example.spot_price
+}
+
+data "aws_security_group" "selected" {
+  id = "output"
+}
+
+output "sgid" {
+  value = date.aws_security_group.selected.id
 }
